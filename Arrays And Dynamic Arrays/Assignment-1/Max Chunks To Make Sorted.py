93a07f4c3701dd03327e66ca9e1acadda41ccc86
Bhanu Pratap <techieonmail@gmail.com>
2022-01-14 20:52:17 +0530

    .

--- Arrays And Dynamic Arrays/Assignment-1/Max Chunks To Make Sorted.py
+++ Arrays And Dynamic Arrays/Assignment-1/Max Chunks To Make Sorted.py
@@ -1,14 +1,15 @@
 # Problem: https://leetcode.com/problems/max-chunks-to-make-sorted/
 
-arr = [int(i) for i in input().split()]
+class Solution:
+    def maxChunksToSorted(self, arr: List[int]) -> int:
 
-count_max = -10**9
+        count_max = -10**9
 
-count = 0
+        count = 0
 
-for i in range(len(arr)):
-    count_max = max(count_max, arr[i])
-    if count_max == i:
-        count += 1
+        for i in range(len(arr)):
+            count_max = max(count_max, arr[i])
+            if count_max == i:
+                count += 1
 
-print(count)
+        return count
--- Arrays And Dynamic Arrays/Assignment-1/Rotate Array.py
+++ Arrays And Dynamic Arrays/Assignment-1/Rotate Array.py
@@ -1,1 +1,13 @@
 # Problem: https://leetcode.com/problems/rotate-array/
+
+nums = [int(i) for i in input().split()]
+k = int(input())
+n = len(nums)
+tmp = nums[:]
+
+for i in range(n):
+    tmp[(i + k) % n] = nums[i]
+
+nums = tmp[:]
+
+print(nums)
